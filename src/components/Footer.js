f1530ff4bb650b680246492eb9bf4e5f8dfd0790
Myself-Tushar <active10100@gmail.com>
2024-12-17 21:42:37 +0530

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="footer bg-gray-800 fixed-footer">
-      <div className="container text-center">
-        <p>&copy; 2024 Tech TSR Recipe App. All rights reserved.</p>
+    <footer className="footer bg-gray-800 fixed-footer w-full">
+      <div className="flex justify-center items-center w-full">
+        <p className="whitespace-nowrap text-white text-center mx-auto">&copy; 2024 Tech TSR Recipe App. All rights reserved.</p>
       </div>
     </footer>
   );
